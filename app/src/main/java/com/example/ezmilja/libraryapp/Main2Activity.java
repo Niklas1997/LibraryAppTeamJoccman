ffbfa9d96f58e57aa3504d1bb18ea8ac748fafbb
Ezedmco <conal.edmundson@ericsson.com>
2017-07-11 10:57:43 +0100

    book list button code and back button code

--- app/src/main/java/com/example/ezmilja/libraryapp/Main2Activity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/Main2Activity.java
@@ -1,13 +1,29 @@
 package com.example.ezmilja.libraryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private Button btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        btn_back = (Button) findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/ezmilja/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.ezmilja.libraryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_booklist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn_booklist = (Button) findViewById(R.id.btn_booklist);
+        btn_booklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
