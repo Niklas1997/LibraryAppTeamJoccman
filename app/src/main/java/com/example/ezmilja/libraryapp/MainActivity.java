7b3b5fc24a5339426c9a241090636c2bc38e30ec
ClareOB <clare.o.brien@ericsson.com>
2017-07-11 12:06:46 +0100

    Clare and James made the front page and it is beautiful

--- app/src/main/java/com/example/ezmilja/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.ezmilja.libraryapp;
 
+import android.graphics.Typeface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         createButton();
+
+
     }
 
     private void createButton(){
@@ -31,3 +35,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
