36b362a6fbf1115b3b24a9645072900a63fa51d3
elundni <niklas.lundberg@ericsson.com>
2017-07-11 14:59:31 +0100

    old versio

--- app/src/main/java/com/example/ezmilja/libraryapp/Main2Activity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/Main2Activity.java
@@ -17,7 +17,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        //
         createListVeiw();
         createButton();
     }
