2aaa6276c7e0142372551b72c3944b47d095a6bd
elundni <niklas.lundberg@ericsson.com>
2017-07-12 16:23:08 +0100

    Search bar works "ok"

--- app/src/main/java/com/example/ezmilja/libraryapp/CustomList.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/CustomList.java
@@ -6,18 +6,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class CustomList extends ArrayAdapter<String>{
+public class CustomList extends ArrayAdapter<String> implements Filterable{
 
     private final Activity context;
     private final String[] book_name;
     private final String[] author;
+    private final String[] books;
     //private final Integer[] imageId;
-    public CustomList(Activity context,
+
+    public CustomList(Activity context, String[] books,
                       String[] book_name, String[] author ) {
-        super(context, R.layout.list_single, book_name);
+        super(context, R.layout.list_single, books);
+        this.books = books;
         this.context = context;
         this.book_name = book_name;
         this.author = author;
@@ -31,12 +36,19 @@ public class CustomList extends ArrayAdapter<String>{
         TextView txtBookName = (TextView) rowView.findViewById(R.id.txt_bookname);
         TextView txtAuthor = (TextView) rowView.findViewById(R.id.txt_author);
 
+
+
+        String[] tempName = super.getItem(position).split(" /n/split/a/ ");
+
         //ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
-        txtBookName.setText(book_name[position]);
-        txtAuthor.setText(author[position]);
+        txtBookName.setText(tempName[0]);
+        txtAuthor.setText(tempName[1]);
 
         //imageView.setImageResource(imageId[position]);
 
         return rowView;
     }
+
 }
+
+
