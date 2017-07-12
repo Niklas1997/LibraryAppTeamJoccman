05ab25443864fe6b7ade871aecc194f6900c3136
ezmilja <james.milton@ericsson.com>
2017-07-12 16:31:08 +0100

    I DID A FONT (in the contents(again(and also in contents("nothing better than a curved sausage"))))

--- app/src/main/java/com/example/ezmilja/libraryapp/CustomList.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/CustomList.java
@@ -6,23 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class CustomList extends ArrayAdapter<String> implements Filterable{
+public class CustomList extends ArrayAdapter<String>{
 
     private final Activity context;
     private final String[] book_name;
     private final String[] author;
-    private final String[] books;
     //private final Integer[] imageId;
-
-    public CustomList(Activity context, String[] books,
+    public CustomList(Activity context,
                       String[] book_name, String[] author ) {
-        super(context, R.layout.list_single, books);
-        this.books = books;
+        super(context, R.layout.list_single, book_name);
         this.context = context;
         this.book_name = book_name;
         this.author = author;
@@ -36,19 +31,12 @@ public class CustomList extends ArrayAdapter<String> implements Filterable{
         TextView txtBookName = (TextView) rowView.findViewById(R.id.txt_bookname);
         TextView txtAuthor = (TextView) rowView.findViewById(R.id.txt_author);
 
-
-
-        String[] tempName = super.getItem(position).split(" /n/split/a/ ");
-
         //ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
-        txtBookName.setText(tempName[0]);
-        txtAuthor.setText(tempName[1]);
+        txtBookName.setText(book_name[position]);
+        txtAuthor.setText(author[position]);
 
         //imageView.setImageResource(imageId[position]);
 
         return rowView;
     }
-
 }
-
-
--- app/src/main/java/com/example/ezmilja/libraryapp/ListActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/ListActivity.java
@@ -1,6 +1,7 @@
 package com.example.ezmilja.libraryapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
