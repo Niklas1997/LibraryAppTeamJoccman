b9e7ccb6389117fc48f77e1854778e769a4b873e
ezmilja <james.milton@ericsson.com>
2017-07-12 15:39:21 +0100

    I DID A FONT (in the contents)

--- app/src/main/java/com/example/ezmilja/libraryapp/ContentsActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/ContentsActivity.java
@@ -1,14 +1,17 @@
 package com.example.ezmilja.libraryapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ContentsActivity extends AppCompatActivity {
 
     private Button btn_list;
+    private Button btn31list;
 
 
 
@@ -18,6 +21,10 @@ public class ContentsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contents);
 
         createButton();
+
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+        btn31list = (Button) findViewById(R.id.btn31list);
+        btn31list.setTypeface(myTypeFace1);
     }
 
     private void createButton(){
