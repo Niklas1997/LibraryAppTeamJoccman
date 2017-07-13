7e2fbae1bff6db320218ba2ba05cb2fada1799fb
ezmilja <james.milton@ericsson.com>
2017-07-13 11:55:28 +0100

    I DID A FONT (in the contents(again(and also in contents("nothing better than a curved sausage"))))

--- app/src/main/java/com/example/ezmilja/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.ezmilja.libraryapp;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_booklist;
+
 
 
 
