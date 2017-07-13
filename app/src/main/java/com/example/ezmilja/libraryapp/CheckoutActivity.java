3a5d6f5ff204aa15e9ae5a0acf3b0cf701f1bf2f
ezmilja <james.milton@ericsson.com>
2017-07-13 11:25:25 +0100

    I DID A FONT (in the contents(again(and also in contents("nothing better than a curved sausage"))))

--- app/src/main/java/com/example/ezmilja/libraryapp/CheckoutActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/CheckoutActivity.java
@@ -1,13 +1,27 @@
 package com.example.ezmilja.libraryapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import static com.example.ezmilja.libraryapp.R.id.button;
+
+
 
 public class CheckoutActivity extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+        button = (Button) findViewById(R.id.button);
+        button.setTypeface(myTypeFace1);
     }
 }
