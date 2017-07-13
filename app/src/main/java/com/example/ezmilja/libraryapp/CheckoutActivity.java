7bb5d7dd4aa2e0155b53318b18da44e8e9dd92e3
broao <aoife.broderick@ericsson.com>
2017-07-13 16:06:23 +0100

    info text box changed

--- app/src/main/java/com/example/ezmilja/libraryapp/CheckoutActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/CheckoutActivity.java
@@ -1,14 +1,19 @@
 package com.example.ezmilja.libraryapp;
 
+import android.app.AlertDialog;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class CheckoutActivity extends AppCompatActivity {
 
     private Button button;
+    private ImageButton btn_info;
 
 
     @Override
@@ -18,10 +23,25 @@ public class CheckoutActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
+        createButton();
+    }
 
-        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+
+    private void createButton() {
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(), "yourfont.ttf");
         button = (Button) findViewById(R.id.button);
         button.setTypeface(myTypeFace1);
+
+
+        btn_info = (ImageButton) findViewById(R.id.imageButton3);
+        btn_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog alertDialog = new AlertDialog.Builder(CheckoutActivity.this).setTitle("Please use the ISBN-13 on back of Book").setIcon(R.drawable.picatyre).setNeutralButton("Close", null).show();
+                button.setTextSize(20);
+            }
+        });
     }
 
 
