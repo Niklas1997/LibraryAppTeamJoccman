860bbb6dbaba1ab8302771e537445341ea00b8dd
ezmilja <james.milton@ericsson.com>
2017-07-13 11:03:50 +0100

    I DID A FONT (in the contents(again(and also in contents("nothing better than a curved sausage"))))

--- app/src/main/java/com/example/ezmilja/libraryapp/RequestActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/RequestActivity.java
@@ -1,13 +1,43 @@
 package com.example.ezmilja.libraryapp;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.R.attr.button;
 
 public class RequestActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
+
+        createButton();
+
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+        TextView editText = (TextView) findViewById(R.id.editText);
+        editText.setTypeface(myTypeFace1);
+
+
+        TextView editText2 = (TextView) findViewById(R.id.editText2);
+        editText2.setTypeface(myTypeFace1);
+
+
+        TextView editText3 = (TextView) findViewById(R.id.editText3);
+        editText3.setTypeface(myTypeFace1);
     }
-}
+
+    private void createButton(){
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+        button = (Button) findViewById(R.id.button);
+        button.setTypeface(myTypeFace1);
+            }
+        }
+
