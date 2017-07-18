8545960d99bcae2f2ae70e9c7aad2fede1d3581e
ezmilja <james.milton@ericsson.com>
2017-07-18 15:04:35 +0100

    votes and that

--- app/src/main/java/com/example/ezmilja/libraryapp/LeaderboardList.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/LeaderboardList.java
@@ -1,30 +1,127 @@
 package com.example.ezmilja.libraryapp;
 
-import android.content.Intent;
+import android.app.Dialog;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LeaderboardList extends AppCompatActivity {
-
+    private final RequestCache books = RequestCache.CACHE;
     private Button buttonRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard_list);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         createButton();
+        makeListView();
+
+    }
+
+    private void makeListView(){
+
+        ListView listView = (ListView)findViewById(R.id.leaderbd_list);
+
+        LeaderboardList.CustomAdapter customAdapter = new LeaderboardList.CustomAdapter();
+        listView.setAdapter(customAdapter);
+
+
+
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                makeRequestDialog();
+
+            }
+        });
+
+    }
+
+    private void createButton(){
 
         Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
         buttonRequest = (Button) findViewById(R.id.buttonRequest);
         buttonRequest.setTypeface(myTypeFace1);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+        buttonRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeRequestDialog();
+            }
+        });
+
+    }
+
+    private void makeRequestDialog(){
+        final Dialog dialog = new Dialog(LeaderboardList.this);
+        dialog.setContentView(R.layout.activity_request);
+        dialog.show();
+
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+
+        Button btn_submitRequest = (Button) dialog.findViewById(R.id.btn_submitrequest);
+
+        btn_submitRequest.setTypeface(myTypeFace1);
+
+        btn_submitRequest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return books.getNumberOfBookRequests();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return books.getBookRequest(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.leaderboard_layout, null);
+
+
+            TextView textView_bookName = view.findViewById(R.id.tbx_bookName);
+            TextView textView_vote = view.findViewById(R.id.tbx_voteCount);
+
+            final BookRequest book = books.getBookRequest(i);
+            textView_bookName.setText(book.getBookName());
+            textView_vote.setText(book.getVote());
+
+            return view;
+        }
 
     }
 
@@ -40,24 +137,7 @@ public class LeaderboardList extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-
-
-
-
-    private void createButton(){
-
-        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
-        buttonRequest = (Button) findViewById(R.id.buttonRequest);
-        buttonRequest.setTypeface(myTypeFace1);
-        buttonRequest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LeaderboardList.this, RequestActivity.class);
-                finish();
-                startActivity(intent);
-            }
-        });
-
-    }
-
 }
+
+
+
