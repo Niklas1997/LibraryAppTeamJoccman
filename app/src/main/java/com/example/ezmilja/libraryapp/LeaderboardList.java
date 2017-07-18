c60688f0876fcb9e8d16cb1e480bed5428665a97
Ezedmco <conal.edmundson@ericsson.com>
2017-07-18 10:38:59 +0100

    list link added

--- app/src/main/java/com/example/ezmilja/libraryapp/LeaderboardList.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/LeaderboardList.java
@@ -1,8 +1,11 @@
 package com.example.ezmilja.libraryapp;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class LeaderboardList extends AppCompatActivity {
@@ -13,14 +16,48 @@ public class LeaderboardList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard_list);
+
+        createButton();
+
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+        buttonRequest = (Button) findViewById(R.id.buttonRequest);
+        buttonRequest.setTypeface(myTypeFace1);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
     }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
 
 
     private void createButton(){
 
         Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
-        buttonRequest = (Button) findViewById(R.id.button);
-        buttonRequest.setTypeface(myTypeFace1);}
+        buttonRequest = (Button) findViewById(R.id.buttonRequest);
+        buttonRequest.setTypeface(myTypeFace1);
+        buttonRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LeaderboardList.this, RequestActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+    }
 
 }
