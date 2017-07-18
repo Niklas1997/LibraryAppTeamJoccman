af1f4532d15daa6bb3fb16eb3f5756010f8a4abc
ezmilja <james.milton@ericsson.com>
2017-07-18 09:06:04 +0100

    List

---
+++ app/src/main/java/com/example/ezmilja/libraryapp/LeaderboardList.java
@@ -0,0 +1,13 @@
+package com.example.ezmilja.libraryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeaderboardList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard_list);
+    }
+}
