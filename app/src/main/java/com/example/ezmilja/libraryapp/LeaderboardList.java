5a2ab70216f111c6b2803d03778f3b03d47e9dfe
Ezedmco <conal.edmundson@ericsson.com>
2017-07-18 10:12:25 +0100

    list link added

--- app/src/main/java/com/example/ezmilja/libraryapp/LeaderboardList.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/LeaderboardList.java
@@ -1,13 +1,26 @@
 package com.example.ezmilja.libraryapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LeaderboardList extends AppCompatActivity {
+
+    private Button buttonRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard_list);
     }
+
+
+
+    private void createButton(){
+
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+        buttonRequest = (Button) findViewById(R.id.button);
+        buttonRequest.setTypeface(myTypeFace1);}
+
 }
