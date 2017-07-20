806cc058ef6346e6f4bedadc29f8ddfdbd614120
ezmilja <james.milton@ericsson.com>
2017-07-20 16:52:30 +0100

    Welcome Page

--- app/src/main/java/com/example/ezmilja/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.ezmilja.libraryapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+        TextView TextView1 = (TextView) findViewById(R.id.TextView1);
+        TextView1.setTypeface(myTypeFace1);
+
                 Timer startTimer = new Timer();
                 startTimer.schedule(new TimerTask() {
 
@@ -26,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
                 }, 5000);
     }
+
+
 }
 
 
