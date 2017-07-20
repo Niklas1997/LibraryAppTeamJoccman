a4e1bc201ebd1c0f698af2fd059d82d586b9a285
ezmilja <james.milton@ericsson.com>
2017-07-20 16:11:01 +0100

    Welcome Page

---
+++ app/src/main/java/com/example/ezmilja/libraryapp/WelcomePage.java
@@ -0,0 +1,45 @@
+package com.example.ezmilja.libraryapp;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class WelcomePage extends AppCompatActivity {
+
+    private Button btn_booklist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+        createButton();
+
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+        TextView textView1 = (TextView) findViewById(R.id.TextView1);
+        textView1.setTypeface(myTypeFace1);
+
+
+    }
+
+    private void createButton(){
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(),"yourfont.ttf");
+        btn_booklist = (Button) findViewById(R.id.btn_booklist);
+        btn_booklist.setTypeface(myTypeFace1);
+        btn_booklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomePage.this, ContentsActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
+
