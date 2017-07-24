1787c60172b4ad00e297120b0f3dd5f16e41d2a6
Ezedmco <conal.edmundson@ericsson.com>
2017-07-24 13:10:19 +0100

    spelling fix

--- app/src/main/java/com/example/ezmilja/libraryapp/CheckoutActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/CheckoutActivity.java
@@ -130,7 +130,7 @@ public class CheckoutActivity extends AppCompatActivity {
                                     Toast.makeText(CheckoutActivity.this, "Book not found", Toast.LENGTH_SHORT).show();
                                 }
                                 else if (tempBook.getNumberOfCopys() < 1){
-                                    Toast.makeText(CheckoutActivity.this, "No copys left", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(CheckoutActivity.this, "No copies left", Toast.LENGTH_SHORT).show();
                                 }
                                 else {
                                     Toast.makeText(CheckoutActivity.this, "Book Checked OUT", Toast.LENGTH_SHORT).show();
