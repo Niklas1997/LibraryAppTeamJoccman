87ab351d8e3e7fa3b88671716aee519c7a198a11
elundni <niklas.lundberg@ericsson.com>
2017-07-26 11:50:28 +0100

    can't use -

--- app/src/main/java/com/example/ezmilja/libraryapp/CheckoutActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/CheckoutActivity.java
@@ -252,6 +252,7 @@ public class CheckoutActivity extends AppCompatActivity {
             //image_book.setVisibility(View.VISIBLE);
             txt_name.setVisibility(View.VISIBLE);
             txt_author.setVisibility(View.VISIBLE);
+            scanBtn.setVisibility(View.INVISIBLE);
         }
         catch (Exception e){}
     }
